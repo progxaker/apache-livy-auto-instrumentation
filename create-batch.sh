72ced9c5191c5be256141961f39b68dbd046d65f
Eduard Mielieshkin <proxaker@workmail.com>
2024-06-22 16:07:29 +0300

    Add a script to create a batch session

---
+++ create-batch.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# docker build -t livy-image:v1 .
+# docker run --rm -p 127.0.0.1:8998:8998 -ti livy-image:v1
+
+curl -X POST -H "Content-Type: application/json" -d '{
+  "file": "file:///tmp/project/simple-project-1.0.jar",
+  "className": "SimpleApp",
+  "args": ["--master", "local[*]"]
+}' http://127.0.0.1:8998/batches
+echo
